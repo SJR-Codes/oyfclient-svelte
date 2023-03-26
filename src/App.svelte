9c6d40f442f352876d80e8a69490cabffa05a92e
Samu Reinikainen <samu.reinikainen@gmail.com>
2023-03-26 14:14:09 +0300

    initializing further

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,41 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
+  import oyfLogo from './assets/logo256.png'
   import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
+  <h1>Own Your Files - Svelte client</h1>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    <a href="https://github.com/SJR-Codes/OwnYourFiles_foto" target="_blank" rel="noreferrer">
+      <img src={oyfLogo} class="logo" alt="OYF Logo" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
 
   <div class="card">
     <Counter />
+    <p>
+      Login form comes here...
+    </p>
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    ...or here.
   </p>
 
   <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
+    Click clicker to see some svelte magic
   </p>
 </main>
 
 <style>
   .logo {
-    height: 6em;
+    /*height: 6em;*/
     padding: 1.5em;
     will-change: filter;
     transition: filter 300ms;
   }
   .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
   }
   .read-the-docs {
     color: #888;
