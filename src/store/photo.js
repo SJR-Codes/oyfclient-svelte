ea34da4f481abceeb471c2eb2ec1aecceea1ee04
Samu Reinikainen <samu.reinikainen@gmail.com>
2023-04-12 17:56:35 +0300

    testing auto logout due session time limit

--- src/store/photo.js
+++ src/store/photo.js
@@ -1,5 +1,6 @@
 import {baseURL} from "../lib/connect.js";
 import {getInit} from "../lib/connect.js";
+import { loggedIn } from '../store/misc.js';
 
 const getPhotoStore = {
   getPhoto: (photo_id) => {
@@ -9,13 +10,17 @@ const getPhotoStore = {
       req = "/photos/"  + photo_id;
     }
     else {
-        throw new Error(`Error! ID missing.`);
+      throw new Error(`Error! ID missing.`);
     }
     const request = new Request(baseURL + req, myInit);
     let apicall = fetch(request)
     .then(function(response) {
       if (!response.ok) {
-              throw new Error(`HTTP error! status: ${response.status}`);
+        //log out if response is unauthorized
+        if (response.status == 401) {
+          loggedIn.set(false);
+        }
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
       return response.json();
     });
