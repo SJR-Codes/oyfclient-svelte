195d6d4149e2544fe731c79681d7ee239d4e2aba
Samu Reinikainen <samu.reinikainen@gmail.com>
2023-04-13 16:23:00 +0300

    category delete added

--- src/categories_setting.svelte
+++ src/categories_setting.svelte
@@ -2,6 +2,7 @@
   import { baseURL } from "../src/lib/connect.js";
   import { getInit } from "../src/lib/connect.js"; 
   import categoryStore from './store/categories.js';
+  import deleteBtn from '/delete.png'
   let allCategories = categoryStore.getAllCategories();
   let title;
   let errormsg;
@@ -10,9 +11,9 @@
     console.debug("Add new category: " + title);
 
     if( !title ) {
-        errormsg = "Error! Enter title for new category.";
-        console.error(errormsg)
-        return null;
+      errormsg = "Error! Enter title for new category.";
+      console.error(errormsg)
+      return null;
     }
 
     let data = JSON.stringify({"title": title});
@@ -21,12 +22,32 @@
     let res = fetch(request)
     .then(function(response) {
       if (!response.ok) {
-              throw new Error(`HTTP error! status: ${response.status}`);
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
       errormsg = "";
       title = "";
       allCategories = categoryStore.getAllCategories();      
     });
+  }
+  function delCategory(id) {
+    console.debug("Delete category: " + id);
+    
+    let myInit = getInit("DELETE", "application/json", "application/json", true);
+    const request = new Request(baseURL + "/categories/" + id, myInit);
+    let res = fetch(request)
+    .then(function(response) {
+      if (!response.ok) {
+        if (response.status == 409) {
+          errormsg = "Notice! Category not removed.<br>(Photos connected to it)";
+          console.error(errormsg)
+          return null;
+        }
+        //throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      console.debug(response);
+      allCategories = categoryStore.getAllCategories();      
+    });
+
   }
 
 </script>
@@ -37,15 +58,18 @@
   {:then categories}
 
     <h3>Categories:</h3>
-    <ul>
-      {#each categories as { id, title }}
-        <li>{title}</li>
-      {/each}
-    </ul>
     <div>
-      <p hidden={!errormsg} style="color: red">{errormsg}</p>
-      <input bind:value={title} type="text" placeholder="Add New Category">
-      <br><br>
+      {#each categories as { id, title }}
+        <div class="listdiv">{title}
+          <div>
+            <img src="{deleteBtn}" alt="Delete category"  on:click={() => delCategory(id)}/>
+          </div>          
+        </div>
+      {/each}
+    </div>
+    <div>
+      <p hidden={!errormsg} style="color: red">{@html errormsg}</p>
+      <input bind:value={title} type="text" placeholder="Add New Category">      
       <button class="button" on:click|preventDefault={addCategory}>add</button>
     </div>
 
@@ -54,7 +78,24 @@
   {/await}
 </div>
 
-<style>
-  div{margin-bottom: 1em;}
+<style>  
+  .listdiv{
+    width: 80%;
+    border: solid 1px #2e2d2d; 
+    border-radius: 1em; 
+    padding: 0.4em;
+    margin: 1em;
+    background-color: #b0b5b3;
+    display: flex;
+  }
+  .listdiv div {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: right;
+  }
   input {border: solid 1px #2e2d2d; border-radius: 1em; padding: 1em; background-color: #b0b5b3;}
+  img {
+    cursor: pointer;
+  }
 </style>
